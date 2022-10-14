77ed22701b7fb9832c9e7627d5299f3d9dc744e1
Stefan Caras <stefan.caras@gmail.com>
2022-10-14 10:45:17 +0300

    update

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const piano = document.querySelector("#piano")
+const array = [0];
+const sounds = () => {
+    for (let i = 1; i < 8; i++) {
+        array[i] = new Audio(`./sounds/a${i}.wav`)
+    }
+}
+sounds();
 
 const playSound = (e) => {
     if (e.target.id === "a1") {
-        const audio = new Audio('./sounds/a1.wav');
-        audio.play();
+        array[1].play();
     } else if (e.target.id === "a2") {
-        const audio = new Audio('./sounds/a2.wav');
-        audio.play();
+        array[2].play();
     } else if (e.target.id === "a3") {
-        const audio = new Audio('./sounds/a3.wav');
-        audio.play();
+        array[3].play();
     } else if (e.target.id === "a4") {
-        const audio = new Audio('./sounds/a4.wav');
-        audio.play();
+        array[4].play();
     } else if (e.target.id === "a5") {
-        const audio = new Audio('./sounds/a5.wav');
-        audio.play();
+        array[5].play();
     } else if (e.target.id === "a6") {
-        const audio = new Audio('./sounds/a6.wav');
-        audio.play();
+        array[6].play();
     } else if (e.target.id === "a7") {
-        const audio = new Audio('./sounds/a7.wav');
-        audio.play();
+        array[7].play();
     }
+    sounds();
 }
 piano.addEventListener("click", playSound)
