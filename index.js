0a149595df961b6c29775bdf294faa56443ecb82
Stefan Caras <stefan.caras@gmail.com>
2023-02-15 09:32:15 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const piano = document.querySelector("#piano")
 const array = [0];
 
 for (let i = 1; i < 8; i++) {
-    array[i] = new Audio(`./sounds/a${i}.mp3`)
+    array[i] = new Audio(`./sounds/a${i}.mp3`);
 }
 
-const playSound = (e) => {
-    if (e.target.id === "a1") {
-        array[1].play();
-        array[1] = new Audio(`./sounds/a1.mp3`)
-    } else if (e.target.id === "a2") {
-        array[2].play();
-        array[2] = new Audio(`./sounds/a2.mp3`)
-    } else if (e.target.id === "a3") {
-        array[3].play();
-        array[3] = new Audio(`./sounds/a3.mp3`)
-    } else if (e.target.id === "a4") {
-        array[4].play();
-        array[4] = new Audio(`./sounds/a4.mp3`)
-    } else if (e.target.id === "a5") {
-        array[5].play();
-        array[5] = new Audio(`./sounds/a5.mp3`)
-    } else if (e.target.id === "a6") {
-        array[6].play();
-        array[6] = new Audio(`./sounds/a6.mp3`)
-    } else if (e.target.id === "a7") {
-        array[7].play();
-        array[7] = new Audio(`./sounds/a7.mp3`)
+document.querySelector("#piano").addEventListener("click", (event) => {
+    const id = event.target.dataset.id;
+    if (id) {
+        array[id].play();
+        array[id] = new Audio(`./sounds/a${id}.mp3`);
     }
-}
-piano.addEventListener("click", playSound)
+})
